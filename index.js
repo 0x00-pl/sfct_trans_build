04017d8ea223f9e1b90d3e0fc66b6ef00ba3b67d
pl <0x00.pl@gmail.com>
2018-01-15 17:52:44 +0800

    add more code

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 let fs = require('fs')
 let path = reqiure('path')
+let mongodb = require('mongodb')
 let { Promise } = require('es6-promise')
+let child_process = require('child_process')
+let process = require('process')
 
 
-// TODO
+function get_file_list_in_path(path, config){
+    return new Promise((resolve, reject)=>{
+	fs.readdir(path, (err, file_list)=>{
+	    if(err) { reject(err) }
+	    else { resolve(file_list) }
+	})
+    })
+}
+
+function get_file_list_is_dir(path){
+    return new Promise((resolve, reject)=>{
+	fs.stat(path, (err, stat)=>{
+	    if(err) { reject(err) }
+	    else { resolve(stat.isDirectory()) }
+	})
+    })
+}
+
+function get_file_list_in_path_rec(path, config){
+    get_file_list_in_path(path, config).then(file_list=>{
+	return Promise.all(file_list.map(file_name=>{
+	    let file_path = path.join(path, file_name)
+	    return get_file_list_is_dir(file_path).then(isdir=>{
+		if(isdir) { return get_file_list_in_path_rec(file_path, config) }
+		else { return file_path }
+	    }).then(file_tree=>Array.prototype.concat(...file_tree))
+	})
+    })
+}
+
 function get_file_list(src_path, config){
-    "return relative path base on src_path and dst_path."
-    return []
+    "return .v file relative path base on src_path and dst_path."
+    return get_file_list_in_path_rec(src_path, config).then(file_list=>{
+	return file_list.filter(file_path=>file_path.endswith('.v'))
+    }).then(file_list=>{
+	return file_list.map(file_path=>path.relative(src_path, file_path))
+    })
 }
 
 function read_file(src_path){
@@ -38,14 +74,17 @@ function trans_file(file_path, src_path, dst_path, db){
     }).then(transed_block_list=>write_file(dstf, transed_block_list.join('')))
 }
 
-// TODO
 function make_html(dst_path){
-    return 'make'
+    return new Promise((resolve, reject)=>{
+	child_process.exec('make', {cwd: dst_path}, (err, stdout, stderr)=>{
+	    if(err) { reject(err) }
+	    else { resolve([stdout, stderr]) }
+	})
+    })
 }
 
-// TODO
 function connect_db(db_config){
-    return 'db'
+    return mongodb.connect(db_config.mongourl)
 }
 // TODO
 function reinit_db(db, src_path, db_config, config){
